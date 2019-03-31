f37cac903ebdf39fe194c6509757484a511002cd
maski07 <energym795213@gmail.com>
2019-03-31 19:56:55 +0900

    fix css by STATICFILES_DIRS

--- manager_project/settings.py
+++ manager_project/settings.py
@@ -134,6 +134,8 @@ USE_L10N = True
 
 USE_TZ = True
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
@@ -141,5 +143,5 @@ STATIC_URL = '../manager/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, '../manager/static'),
 )
