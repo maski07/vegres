ec9c1ce560b1d2b1d33f6958eb14891f792c3942
maski07 <energym795213@gmail.com>
2019-04-01 23:11:58 +0900

    dubug=true

--- manager_project/settings.py
+++ manager_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'hmludgw!ju+2w=qh1=1j^lur%a_8dqu&@+do$+$ekn*(+tb_=y'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
